62a0a5a88ce1f68d7fa3f10c54aa3824f9a1d5d2
Tim White <35063371+itstimwhite@users.noreply.github.com>
2025-08-06 16:29:07 -0700

    chore: remove outdated social bar (#102)

--- components/molecules/index.ts
+++ components/molecules/index.ts
@@ -2,3 +2,4 @@
 // Simple combinations of atoms functioning together
 
 export * from './AuthActions';
+export * from './SocialLink';
---
+++ components/organisms/SocialBar.tsx
@@ -0,0 +1,25 @@
+import { SocialLink } from '@/components/molecules/SocialLink';
+import type { SocialLink as SocialLinkType } from '@/types/db';
+
+interface SocialBarProps {
+  handle: string;
+  artistName: string;
+  socialLinks: SocialLinkType[];
+}
+
+export function SocialBar({ handle, artistName, socialLinks }: SocialBarProps) {
+  return (
+    <div
+      className={`flex flex-wrap justify-center gap-3 ${socialLinks.length === 0 ? 'hidden' : ''}`}
+    >
+      {socialLinks.map((link) => (
+        <SocialLink
+          key={link.id}
+          link={link}
+          handle={handle}
+          artistName={artistName}
+        />
+      ))}
+    </div>
+  );
+}
